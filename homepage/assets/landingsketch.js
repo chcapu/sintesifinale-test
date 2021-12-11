c7534e453ffcbb02304ffe40abdfedf12524de30
chcapu <chc.caputo@gmail.com>
2021-12-11 17:43:50 +0100

    matter.js interaction sketch added
    aggiunto sketch delle robe che seguono il mouse così com'era in /landing. con immagini provvisorie

--- homepage/assets/landingsketch.js
+++ homepage/assets/landingsketch.js
@@ -1,22 +1,117 @@
 //Credits:
 //reference for the animated text: https://openprocessing.org/sketch/825026
 
+Matter.use("matter-attractors");
 
 //GLOBAL VARIABLES:
+
 //for the animated text
 let t = 0;
 let textArray = ["ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?"];
 let textIndex = 0;
 let horSpace = 30;
+
 //for the pixel texture effect
 let noiseGra;
 
+//for matter.js interaction
+let Engine = Matter.Engine,
+  World = Matter.World,
+  Body = Matter.Body,
+  Bodies = Matter.Bodies,
+  Events = Matter.Events,
+  Runner = Matter.Runner,
+  Render = Matter.Render,
+  Composite = Matter.Composite,
+  Composites = Matter.Composites,
+  Constraint = Matter.Constraint,
+  Mouse = Matter.Mouse,
+  MouseConstraint = Matter.MouseConstraint;
+
+let image1, image2, image3, image4;
+let canvas;
+let canvasmouse;
+
+let engine;
+let world;
+let box1;
+let boxes = [];
+let attractiveBody;
+
+let boundaries = [];
+let myImages = [];
+let rx, ry, rz;
+
+let box2;
+let mConstraint;
+let render;
+let mouse;
+
+/////////////////////////////////////////////////////////////////
+
 function preload(){
+  image1 = loadImage("./assets/images/ehi.jpg");
+  image2 = loadImage("./assets/images/2.JPG");
+  image3 = loadImage("./assets/images/1.jpeg");
+  image4 = loadImage("./assets/images/4.jpg");
 }
 
+/////////////////////////// SETUP //////////////////////////////
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  canvas = createCanvas(windowWidth,windowHeight);
   background("rgb(245,245,245)");
+
+  //matter.js interaction : setup
+  engine = Engine.create();
+  myImages.push(image1, image2, image3, image4);
+  let runner = Runner.create();
+  world = engine.world;
+  engine.world.gravity.y = 0.2;
+  Matter.Runner.run(engine);
+
+  push()
+  boundaries.push(new Boundary(width / 2, height, width, 10, 0));
+  boundaries.push(new Boundary(width / 2, 0, width, 10, 0));
+  boundaries.push(new Boundary(0, height / 2, 10, height, 0));
+  boundaries.push(new Boundary(width, height / 2, 10, height, 0));
+  pop()
+
+  render = Render.create({
+    element: canvas.elt,
+    engine: engine,
+    options3: {
+      showVelocity: false,
+      width: 100,
+      height: 100,
+      wireframes: false,
+      background: "rgb(240,240,240)",
+    },
+  });
+  World.add(world, render);
+
+  mouse = Mouse.create(render.canvas.elt); //add mouse control
+
+  attractiveBody = Bodies.circle(mouseX, mouseY, 10, {
+    render: {
+      fillStyle: `rgb(240,240,240)`,
+      strokeStyle: `rgb(240,240,240)`,
+      lineWidth: 0,
+    },
+    isStatic: true,
+    plugin: {
+      attractors: [
+        function (bodyA, bodyB) {
+          return {
+            x: (bodyA.position.x - bodyB.position.x) * 1e-6,
+            y: (bodyA.position.y - bodyB.position.y) * 1e-6,
+          };
+        },
+      ],
+    },
+  });
+  World.add(world, attractiveBody);
+
+
 
   //text settings for animated array : setup
   textFont("IBM Plex Mono");
@@ -34,11 +129,14 @@ function setup() {
 	} noiseGra.updatePixels();
 }
 
+
+/////////////////////////// DRAW //////////////////////////////
 function draw() {
   push();
   blendMode(HARD_LIGHT);
   background("rgba(245,245,245,1)");
   pop();
+
   //animated text array effect : draw
   t++;
   let textWid = textWidth(textArray[textIndex]);
@@ -53,6 +151,21 @@ function draw() {
       text(textArray[textIndex][b], horSpace*b+a*dxOff, (a*40));
     }
   }; pop();
+
+
+  //matter.js interaction : draw
+  if (boxes.length < 3) {
+    for (let i = 0; i < myImages.length; i++) {
+    boxes.push(new Box(myImages[i], windowWidth/2, windowHeight/2, 100, 80));
+    }
+  }
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+  }
+
+  moveit();
+
 
   //pixel texture effect : draw
   //image(noiseGra, 0, 0);
