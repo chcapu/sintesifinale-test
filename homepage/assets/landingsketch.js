c5ddcaabbe6104a38eb7ff555cd09f8da622f991
chcapu <chc.caputo@gmail.com>
2021-12-11 16:26:51 +0100

    noise grain effect added

--- homepage/assets/landingsketch.js
+++ homepage/assets/landingsketch.js
@@ -2,28 +2,44 @@
 //reference for the animated text: https://openprocessing.org/sketch/825026
 
 
+//GLOBAL VARIABLES:
+//for the animated text
 let t = 0;
 let textArray = ["ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?  ARE YOU STILL WATCHING?"];
 let textIndex = 0;
 let horSpace = 30;
+//for the pixel texture effect
+let noiseGra;
 
 function preload(){
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  background("white");
+  background("rgb(245,245,245)");
 
-  //text settings for animated array
+  //text settings for animated array : setup
   textFont("IBM Plex Mono");
   textSize(20);
   textStyle(NORMAL);
   textAlign(CENTER);
+
+  //pixel texture effect : setup
+  noiseGra = createGraphics(windowWidth, windowHeight);
+	noiseGra.loadPixels()
+	for( let  x=0; x<=width; x++){
+		for(let y=0; y<=height; y++){
+      noiseGra.set(x, y, color(200, noise(x/10,y/10,x*y/50)*random([0,40,80])))
+		}
+	} noiseGra.updatePixels();
 }
 
 function draw() {
-  //animated text array effect
-  background("white");
+  push();
+  blendMode(HARD_LIGHT);
+  background("rgba(245,245,245,1)");
+  pop();
+  //animated text array effect : draw
   t++;
   let textWid = textWidth(textArray[textIndex]);
   push();
@@ -37,6 +53,9 @@ function draw() {
       text(textArray[textIndex][b], horSpace*b+a*dxOff, (a*40));
     }
   }; pop();
+
+  //pixel texture effect : draw
+  image(noiseGra, 0, 0);
 }
 
 function windowResized() {
