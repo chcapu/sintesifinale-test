0401a39fd710b98ff0ab72eaffc5c81994127cdd
chcapu <chc.caputo@gmail.com>
2021-12-11 13:00:46 +0100

    edit

---
+++ landingsketch.js
@@ -0,0 +1,12 @@
+function preload(){
+  // put preload code here
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  ellipse(100,100,100,100);
+}
+
+function draw() {
+  // put drawing code here
+}
