ea7100a5f82c4214bdb3921e9c0ba6b58ff00c3f
chcapu <chc.caputo@gmail.com>
2021-12-13 16:18:52 +0100

    fatigue test start

---
+++ decisionfatigue/assets/fatiguesketch.js
@@ -0,0 +1,27 @@
+//Credits:
+//reference:
+
+//GLOBAL VARIABLES:
+
+
+
+/////////////////////////////////////////////////////////////////
+function preload(){
+}
+
+/////////////////////////// SETUP //////////////////////////////
+function setup() {
+  canvas = createCanvas(windowWidth,windowHeight);
+  background("rgb(245,245,245)");
+
+}
+
+
+/////////////////////////// DRAW //////////////////////////////
+function draw() {
+
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
