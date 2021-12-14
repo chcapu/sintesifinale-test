a15a7150619f3669bc61b6cacf9e259bba352359
chcapu <chc.caputo@gmail.com>
2021-12-14 13:11:00 +0100

    update ref codepen funza

--- decisionfatigue-prova2/assets/fatiguesketch.js
+++ decisionfatigue-prova2/assets/fatiguesketch.js
@@ -1,57 +1,43 @@
 //Credits:
-//reference:
+//reference: https://codepen.io/rkgttr/pen/ZGZzJG
 
-//GLOBAL VARIABLES:
-let canvas;
-let thumbnails = [];
-let particles = [];
-let attractor;
+let x = 0,
+    y = 0,
+    strength = 1000, // higher is stronger
+    mouseMoveHandler = function(e) {
+      x = e.pageX;
+      y = e.pageY;
+    },
+    points = Array.from(document.querySelectorAll('circle'), el => {
+      return {
+        circle: el,
+        x:Number(el.getAttribute('cx')),
+        y:Number(el.getAttribute('cy')),
+        ox:Number(el.getAttribute('cx')),
+        oy:Number(el.getAttribute('cy'))
+      };
+    }),
+    animate = function(){
+      let dx,
+          dy,
+          dist,
+          angle;
+      points.forEach( ( el, i ) => {
+        // start repulsion calculation
+        dx = el.x - x;
+        dy = el.y - y;
+        angle = Math.atan2( dy, dx );
+        dist = strength / Math.sqrt( dx * dx + dy * dy );
+        el.x += Math.cos( angle ) * dist;
+        el.y += Math.sin( angle ) * dist;
+        el.x += (el.ox - el.x)*.1;
+        el.y += (el.oy - el.y)*.1;
+        // end repulsion calculation
+        el.circle.setAttribute('cx', el.x);
+        el.circle.setAttribute('cy', el.y);
+      });
+      window.requestAnimationFrame(animate);
 
-
-/////////////////////////////////////////////////////////////////
-function preload(){
-  for (let i = 0; i <= 1; i++) {
-    thumbnails[i] = loadImage("./assets/images/thumbnail" + i + ".jpg");
-  }
-}
-
-/////////////////////////// SETUP //////////////////////////////
-function setup() {
-  canvas = createCanvas(windowWidth,windowHeight);
-  background("rgb(245,245,245)");
-}
-
-
-/////////////////////////// DRAW //////////////////////////////
-function draw() {
-  //attractor
-  attractor = createVector(mouseX, mouseY);
-  push();
-  stroke(0, 255, 0);
-  strokeWeight(5);
-  point(attractor.x, attractor.y);
-  pop();
-
-
-  //thumbnails
-  background(51);
-  stroke(255);
-  strokeWeight(4);
-  particles.push(new Thumbnail(random(width), random(height)));
-
-  if (particles.length > 100) {
-    particles.splice(0, 1);
-  }
-
-  for (let i = 0; i < particles.length; i++) {
-    var particle = particles[i];
-    particle.attracted(attractor);
-    particle.update();
-    particle.show();
-  }
-
-}
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
+    };
+window.addEventListener('mousemove', mouseMoveHandler);
+animate();
